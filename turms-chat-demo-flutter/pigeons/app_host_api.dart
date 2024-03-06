5d5921ac0a80c45ecabd7192cd45a9fb950ced84
JamesChenX <eurekajameschen@gmail.com>
2024-03-06 20:05:18 +0800

    wip

---
+++ turms-chat-demo-flutter/pigeons/app_host_api.dart
@@ -0,0 +1,24 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/app_host_api.g.dart',
+  dartOptions: DartOptions(),
+  cppOptions: CppOptions(namespace: 'turms_chat_demo'),
+  cppHeaderOut: 'windows/runner/app_host_api.g.h',
+  cppSourceOut: 'windows/runner/app_host_api.g.cpp',
+  swiftOut: 'macos/Runner/AppHostApi.g.swift',
+  dartPackageName: 'turms_chat_demo',
+))
+@HostApi()
+abstract class AppHostApi {
+  DiskSpaceInfo getDiskSpace(String path);
+}
+
+class DiskSpaceInfo {
+  DiskSpaceInfo(
+      {required this.total, required this.free, required this.usable});
+
+  final int total;
+  final int free;
+  final int usable;
+}
